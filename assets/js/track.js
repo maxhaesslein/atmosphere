76fc6a98709a691414fc3496f6ce8760af0a2f1d
maxhaesslein <git@maxhaesslein.de>
2025-05-30 11:51:01 +0200

    removed 'volume' from track for now

--- assets/js/track.js
+++ assets/js/track.js
@@ -1,8 +1,3 @@
-/*
-	<button class="play-pause">Track Play/Pause</button>
-	<label><input class="volume" type="range" min="0" max="100" value="80" step="1"> <span class="range-value">80%</span></label>
-*/
-
 function Track() {
 	return this.createHTMLElement();
 }
@@ -17,15 +12,6 @@ Track.prototype.createHTMLElement = function(){
 
 	const playPause = new Button('Play/Pause', ['play-pause']);
 	element.appendChild(playPause);
-
-	const volume = new Range( 'Volume', {
-		'min': 0,
-		'max': 100,
-		'value': 80,
-		'step': 1,
-		'unit': '%'
-	} );
-	element.appendChild(volume);
 
 	return element;
 }
