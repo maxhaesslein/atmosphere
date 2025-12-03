73f747c5a2bc203653b87f56dac45b95d9cb9f4d
maxhaesslein <git@maxhaesslein.de>
2025-12-03 17:45:14 +0100

    basic pwa support

--- index.php
+++ index.php
@@ -1,28 +1,38 @@
-<!DOCTYPE html>
+<?php
+
+$version = time(); // TODO: add version variable; currently we use this as a cache buster on every reload for development.
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 
+
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-	<meta name="viewport" content="viewport-fit=cover, user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="viewport" content="viewport-fit=contain, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<link rel="manifest" href="manifest.json">
+	<link rel="apple-touch-icon" href="icon-144x144.png">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="default">
 
 	<title>Atmosphere</title>
 
-	<link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/style.css?v=<?= $version ?>" type="text/css" media="all">
 
-	<script type="text/javascript" src="assets/js/app.js" defer></script>
-	<script type="text/javascript" src="assets/js/board.js" defer></script>
-	<script type="text/javascript" src="assets/js/scene.js" defer></script>
-	<script type="text/javascript" src="assets/js/session.js" defer></script>
-	<script type="text/javascript" src="assets/js/track.js" defer></script>
-	<script type="text/javascript" src="assets/js/youtube.js" defer></script>
+	<script type="text/javascript" src="assets/js/app.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/board.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/scene.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/session.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/track.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/youtube.js?v=<?= $version ?>" defer></script>
 	
-	<script type="text/javascript" src="assets/js/elements/button.js" defer></script>
-	<script type="text/javascript" src="assets/js/elements/checkbox.js" defer></script>
-	<script type="text/javascript" src="assets/js/elements/drag-drop-handler.js" defer></script>
-	<script type="text/javascript" src="assets/js/elements/editable-text.js" defer></script>
-	<script type="text/javascript" src="assets/js/elements/range.js" defer></script>
+	<script type="text/javascript" src="assets/js/elements/button.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/elements/checkbox.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/elements/drag-drop-handler.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/elements/editable-text.js?v=<?= $version ?>" defer></script>
+	<script type="text/javascript" src="assets/js/elements/range.js?v=<?= $version ?>" defer></script>
 
 </head>
 <body>
